111bb212b67e369a02c63bfdd27d3e0355c99f50
doishun <2stars.s2@gmail.com>
2016-01-05 17:49:22 +0900

    レシピ更新

--- cookbooks/chef/recipes/default.rb
+++ cookbooks/chef/recipes/default.rb
@@ -1,19 +1,36 @@
 #######################
 # パッケージのインストール
 #######################
-%w{nginx mysql mysql-server}.each do |pkg|
+%w{nginx}.each do |pkg|
   package pkg do
     action :install
   end
 end
 
 
+##########################
+# nodejsの実装
+##########################
+script "install-nodejs" do
+  interpreter "bash"
+  user        "root"
+  code <<-'EOS'
+    sudo yum install -y http://dev.mysql.com/get/mysql-community-release-el6-5.noarch.rpm
+    sudo yum install -y mysql-client mysql-server
+    
+    sudo yum install -y epel-release
+    sudo yum install -y nodejs
+    
+    sudo yum install -y redis
+  EOS
+end
+
+
 #######################
 # サービスの自動起動
 #######################
-%w{nginx mysqld}.each do |srv|
+%w{nginx mysqld redis}.each do |srv|
   service srv do
     action [ :enable, :start ]
   end
 end
-
